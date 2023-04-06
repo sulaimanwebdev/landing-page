54f949a6bfdcb3a2905ba759a8d41461a5b83c27
Muhammad Sulaiman <trendingofyt@gmail.com>
2023-04-06 20:04:20 +0500

    changes added

--- src/App.js
+++ src/App.js
@@ -12,13 +12,13 @@ function App() {
         </div>
 
 
-        <div className="max-w-[900px] mx-auto flex items-center justify-center flex-col pt-12 px-4">
+        <div className="max-w-[900px] mx-auto flex items-center justify-center flex-col pt-12 lg2:pb-32 px-4">
           <img src="/images/logo.svg" alt="logo" className="w-[500px]" />
 
           <div className="text-[38px] font-bold mt-8">Exklusive <span className="text-[#184EFF]">$14</span> No-Deposit Offer 🎁</div>
-          <div className="text-[15px] text-[#5D69BE] font-[500] mt-3">To celebrate the launch of our Sportsbook we have partnered with Blockchair to give all users a FREE$14 Non-Deposit Bonus.</div>
+          <div className="text-[15px] font-[500] mt-3">To celebrate the launch of our Sportsbook we have partnered with Blockchair to give all users a FREE$14 Non-Deposit Bonus.</div>
           
-          <div className="flex items-center gap-1 sm2:gap-7 flex-col sm2:flex-row rounded-xl bg-[#161E56] px-5 pt-4 sm2:pt-0 mt-8">
+          <div className="customShadow2 flex items-center gap-1 sm2:gap-7 flex-col sm2:flex-row rounded-xl bg-[#161E56] px-5 pt-4 sm2:pt-0 mt-8">
             <div className="text-[#5D69BE] font-[500]">The Free Non-Deposit Bonus can be claimed in:</div>
             <img src="/images/coins.svg" alt="coins" className="w-[170px]" />
           </div>
@@ -34,9 +34,16 @@ function App() {
             <div className="text-[18px] font-[600] pl-1.5 mt-1.5">CLAIM FREE $14 CRYPTO!</div>
           </a>
 
-          <div className="customShadow rounded-lg bg-[#161E56] w-full p-2 pt-0 mt-10 mb-32">
+          <div className="customShadow rounded-lg bg-[#161E56] w-full p-2 pt-0 mt-10">
               <div className="text-center text-[#5D69BE] my-3">Join this Channel to claim the Bonus:</div>
               <img src="/images/discord2.png" alt="discord" className="w-full" />
+          </div>
+
+
+          <div className="flex lg2:hidden items-center justify-center flex-wrap gap-8 mt-10 mb-12">
+            <img src="/images/icon1.svg" alt="icon" />
+            <img src="/images/icon2.svg" alt="icon" />
+            <img src="/images/icon3.svg" alt="icon" />
           </div>
 
         </div>
